817d62a7cf5ea76e12d2da6d7335266ef7bc372b
Shinx606 <224807183+mescuatro@users.noreply.github.com>
2025-08-20 22:31:41 -0300

    Mostrar nombres

--- js/script.js
+++ js/script.js
@@ -19,3 +19,9 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch(DATA_URL)
+  .then(response =>  {
+    if (!response.ok) { throw new Error("Falló."); }
+     return response.json() })
+  .then(data => { showData(data.students); })
+  .catch(error => console.error("Hubo un error", error));
